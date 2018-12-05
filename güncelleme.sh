642d028719e6db22a9ad14149fb2738e0876731e
İbrahim Oğuzhan Çeribaşı <40391070+exnovan@users.noreply.github.com>
2018-12-05 18:11:24 +0300

    Add files via upload

---
+++ güncelleme.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+#deneme kodlama
+#05/12/2018
+name='İBRAHİM OĞUZHAN ÇERİBAŞI'
+echo SİZİN İÇİN GÜNCELLEMELERİ BAŞLATIYORUM
+sleep 2
+apt-get update  #repo updatedini yap
+echo REPO GÜNCELLEMESİ BİTTİ SİSTEM GÜNCELLEMESİNE GEÇİYORUM #ekrana yazıdır
+sleep 3 #3 saniye daha bekle
+apt-get upgrade #sistem updatini yap
+sleep 2 #2 saniye daha bekle
+echo SİSTEM GÜNCELLEMESİ BİTTİ,SÜRÜM GÜNCELLEMESİNE GEÇİYORUM #ekrana yazdır
+sleep 2
+apt-get dist-upgrade #sürüm güncellemesi
+echo '                           '
+echo '                           '
+sleep 2
+echo "BÜTÜN GÜNCELLEMELER BİTTİ SCRİPT'İ KULLANDIĞINIZ İÇİN TEŞEKKÜR EDERİM."
+echo $name
